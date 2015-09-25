ae691f597241353c7b990a91235d09b9e3984a47
Chris Roberts <clrobert@uno.edu>
2015-09-25 11:09:14 -0500

    ok

--- scripts/nahbro.js
+++ scripts/nahbro.js
@@ -1,60 +1,82 @@
-token = process.env.HUBOT_SLACK_TOKEN;
-botname = process.env.HUBOT_BOT_NAME;
-baseURL = 'https://slack.com/api';
+# Description:
+#   A simple 'naw dawg' for hubot to cleanup after himself
+#   https://api.slack.com/methods/channels.history/
+#   https://api.slack.com/methods/chat.delete/
+#
+# Configuration:
+#   HUBOT_SLACK_TOKEN
+#
+# Commands:
+#   hubot naw dawg
+#   
+# Dependencies:
+#   lodash
+#
+# Examples:
+#   hubot naw dawg
+#
+# Author:
+#   Matt Shwery (mshwery)
+#   
 
-module.exports = function(robot) {
-  robot.respond(/dank/i, function(msg){
-  var today = new Date();
+_ = require('lodash')
 
-  msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
-
-  robot.http("#{baseURL}/users.list?token=#{token}")(function(err, resp, body) {
-      msg.reply(body);
-  });
-
-});
+baseUrl = 'https://slack.com/api'
+token = process.env.HUBOT_SLACK_TOKEN
+historyLimit = 5
+responses = {
+  error: "I'm sorry, Dave. I'm afraid I can't do that.",
+  success: "My b dawg"
 }
 
-module.exports = function(robot) {
-  robot.respond(/swank/i, function(msg){
-  var today = new Date();
-  msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
+module.exports = (robot) ->
+  hubotName = robot.name
+  channels = []
 
-});
-}
+  robot.respond /naw dawg$/i, (msg) ->
+    channelName = msg.message.room
+    botUser = robot.brain.userForName(hubotName)
 
-//deleteMessage = (robot, channel, ts) ->
-//  robot.http("#{baseURL}/chat.delete?token=#{token}&channel=#{channel}&ts=#{ts}")
-//    .get() (err, res, body) ->
-//      throw err if err
-//      msg.send(res)
-//      msg.send(body)
-//
-//getHistory = (robot, channel, cb) ->
-//  if (channel.substr(0,1) == "G")
-//    robot.http("#{baseURL}/groups.history?token=#{token}&channel=#{channel}&count=15").get() (err, res, history) ->
-//      throw err if err
-//      cb history
-//  else
-//    robot.http("#{baseURL}/channels.history?token=#{token}&channel=#{channel}&count=15").get() (err, res, history) ->
-//      throw err if err
-//      cb history
-//
-//module.exports = (robot) ->
-//  robot.respond /naw bro/i, (msg) ->
-//    msg.send("My b dawg")
-//    count = 1
-//    channel = msg.message.rawMessage.channel
-//
-//    userid = -1
-//    robot.http("#{baseURL}/users.list?token=#{token}").get() (err, res, users) ->
-//      throw err if err
-//      userid = (user for user in users.members when user.name is username)[0]
-//      cb userid.id
-//
-//    getHistory robot, channel, (history) ->
-//      messages = (message for message in history.messages when message.user is hubotid)
-//      messages = messages.slice 0, count
-//      for msg, i in messages
-//        deleteMessage  robot, channel, msg.ts
-//
+
+    deleteMessage = (message, channel) ->
+      url = "#{baseUrl}/chat.delete?token=#{token}&channel=#{channel.id}&ts=#{message.ts}"
+      msg.robot.http(url).get() (err, res, body) ->
+        data = JSON.parse(body)
+        if (data.ok)
+          msg.send responses.success
+        else
+          msg.send responses.error
+
+
+    fetchChannelHistory = () ->
+      channel = _.find(channels, { name: channelName })
+
+      # now that we may have a channel
+      # we can use its id to get that channel's history
+      if (channel)
+        url = "#{baseUrl}/channels.history?token=#{token}&channel=#{channel.id}&count=#{historyLimit}"
+        msg.robot.http(url).get() (err, res, body) ->
+          data = JSON.parse(body)
+          lastBotMessage = _.find(data.messages, { user: botUser.id })
+
+          # if we have a message from this bot,
+          # lets delete it!
+          if lastBotMessage and !_.includes(responses, lastBotMessage.text)
+            deleteMessage(lastBotMessage, channel)
+          else
+            msg.send responses.error
+      else
+        msg.send "/shrug Sorry, I couldn't find this channel correctly"
+
+
+    if !botUser
+      console.warn "Couldn't find a user for #{hubotName}"
+
+    if !channels.length
+      # get a list of channels (unfortunately the only way to get channel IDs for the next api call)
+      msg.robot.http("#{baseUrl}/channels.list?token=#{token}&exclude_archived=1").get() (err, res, body) ->
+        data = JSON.parse(body)
+        channels = data.channels
+        fetchChannelHistory()
+    else
+      fetchChannelHistory()
