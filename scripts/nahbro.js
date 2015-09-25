2a35041f7ffc19f4069a2062b63350a890f9087b
Chris Roberts <clrobert@uno.edu>
2015-09-25 09:37:50 -0500

    trying

--- scripts/nahbro.js
+++ scripts/nahbro.js
@@ -1,7 +1,6 @@
-token = process.env.HUBOT_SLACK_TOKEN
-botname = process.env.HUBOT_BOT_NAME
-baseURL = 'https://slack.com/api'
-
+token = process.env.HUBOT_SLACK_TOKEN;
+botname = process.env.HUBOT_BOT_NAME;
+baseURL = 'https://slack.com/api';
 
 module.exports = function(robot) {
     robot.respond(/is it (weekend|holiday)\s?\?/i, function(msg){
@@ -11,38 +10,38 @@ module.exports = function(robot) {
     });
 }
 
-#deleteMessage = (robot, channel, ts) ->
-#  robot.http("#{baseURL}/chat.delete?token=#{token}&channel=#{channel}&ts=#{ts}")
-#    .get() (err, res, body) ->
-#      throw err if err
-#      msg.send(res)
-#      msg.send(body)
-#
-#getHistory = (robot, channel, cb) ->
-#  if (channel.substr(0,1) == "G")
-#    robot.http("#{baseURL}/groups.history?token=#{token}&channel=#{channel}&count=15").get() (err, res, history) ->
-#      throw err if err
-#      cb history
-#  else
-#    robot.http("#{baseURL}/channels.history?token=#{token}&channel=#{channel}&count=15").get() (err, res, history) ->
-#      throw err if err
-#      cb history
-#
-#module.exports = (robot) ->
-#  robot.respond /naw bro/i, (msg) ->
-#    msg.send("My b dawg")
-#    count = 1
-#    channel = msg.message.rawMessage.channel
-#
-#    userid = -1
-#    robot.http("#{baseURL}/users.list?token=#{token}").get() (err, res, users) ->
-#      throw err if err
-#      userid = (user for user in users.members when user.name is username)[0]
-#      cb userid.id
-#
-#    getHistory robot, channel, (history) ->
-#      messages = (message for message in history.messages when message.user is hubotid)
-#      messages = messages.slice 0, count
-#      for msg, i in messages
-#        deleteMessage  robot, channel, msg.ts
-#
+//deleteMessage = (robot, channel, ts) ->
+//  robot.http("#{baseURL}/chat.delete?token=#{token}&channel=#{channel}&ts=#{ts}")
+//    .get() (err, res, body) ->
+//      throw err if err
+//      msg.send(res)
+//      msg.send(body)
+//
+//getHistory = (robot, channel, cb) ->
+//  if (channel.substr(0,1) == "G")
+//    robot.http("#{baseURL}/groups.history?token=#{token}&channel=#{channel}&count=15").get() (err, res, history) ->
+//      throw err if err
+//      cb history
+//  else
+//    robot.http("#{baseURL}/channels.history?token=#{token}&channel=#{channel}&count=15").get() (err, res, history) ->
+//      throw err if err
+//      cb history
+//
+//module.exports = (robot) ->
+//  robot.respond /naw bro/i, (msg) ->
+//    msg.send("My b dawg")
+//    count = 1
+//    channel = msg.message.rawMessage.channel
+//
+//    userid = -1
+//    robot.http("#{baseURL}/users.list?token=#{token}").get() (err, res, users) ->
+//      throw err if err
+//      userid = (user for user in users.members when user.name is username)[0]
+//      cb userid.id
+//
+//    getHistory robot, channel, (history) ->
+//      messages = (message for message in history.messages when message.user is hubotid)
+//      messages = messages.slice 0, count
+//      for msg, i in messages
+//        deleteMessage  robot, channel, msg.ts
+//
