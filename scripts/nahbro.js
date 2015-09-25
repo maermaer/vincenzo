2c2744e679a5fe5f3c4f68bcd5809a5bd20fd9a6
Chris Roberts <clrobert@uno.edu>
2015-09-25 10:42:58 -0500

    ok

--- scripts/nahbro.js
+++ scripts/nahbro.js
@@ -18,7 +18,7 @@ module.exports = function(robot) {
 }
 
 module.exports = function(robot) {
-  robot.respond(/swank\s?\?/i, function(msg){
+  robot.respond(/swank/i, function(msg){
   var today = new Date();
   msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
 
