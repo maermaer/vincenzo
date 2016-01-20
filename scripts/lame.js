053a001eba674620cc22fb41b3ac85706e3f5979
Chris Roberts <clrobert@uno.edu>
2016-01-20 15:10:48 -0600

    again:

--- scripts/lame.js
+++ scripts/lame.js
@@ -17,7 +17,7 @@
 module.exports = function(robot) {
     robot.respond(/what say you\s?\?/i, function(msg){
 
-        var rand_words = [][];
+        var rand_words = [];
         for(i=0; i<5;i++)
         {
           for(j=0;j<5;j++)
