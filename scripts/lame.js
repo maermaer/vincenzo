0fa2361f2d074e9e5f75afb3f0c57f5b9ba13d49
Chris Roberts <clrobert@uno.edu>
2016-01-20 14:58:56 -0600

    whoops

--- scripts/lame.js
+++ scripts/lame.js
@@ -15,7 +15,7 @@ module.exports = function(robot) {
     robot.respond(/what say you\s?\?/i, function(msg){
         var rand = words[Math.floor(Math.random() * words.length)];
 
-        msg.reply(words[1]);
+        msg.reply(words[rand]);
     });
 }
 
